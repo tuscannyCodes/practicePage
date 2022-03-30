05710996a0c14f047ddb27a99fdc62514e5a49db
Tuscanny Polk <tuscannypolk@gmail.com>
2022-03-30 02:33:15 -0400

    added second lesson part. moving toward data storage and manipulation

--- app.js
+++ app.js
@@ -1,29 +1,35 @@
+// this is lesson 1\/
 // THESE ARE VARIABLES*****
-
 const MainButton = document.getElementById("MainButton");
- 
 const MainText = document.getElementById("MainText");
-
-// console.log(MainButton)
-
 const MainDiv = document.getElementById("MainDiv");
-
+const MainTextContainer = document.getElementById("MainTextContainer");
+const ButtonAndDiv = document.getElementById("ButtonAndDivContainer");
 // console.log(MainDiv)
 
 // lets try to change the text?
 
 // THESE ARE FUNCTIONS*****
 
-
-
-
 function colorAndShapeChange() {
   MainDiv.style.backgroundColor = "cadetblue";
   MainDiv.style.borderRadius = "50px";
-  MainText.innerHTML= "WOW";
-  
+  // this is lesson 1 ^
+  // this is lesson 2\/
+  MainText.innerHTML = "what is your name?";
+  const inputBox = document.createElement("input");
+  inputBox.className = "InputBox";
+  MainTextContainer.appendChild(inputBox);
+
+  // add another button and remove the old one
+  ButtonAndDiv.removeChild(MainButton);
+  const SecondButton = document.createElement("button");
+  SecondButton.className = "MainButton";
+  ButtonAndDiv.appendChild(SecondButton);
+  SecondButton.innerHTML = "ENTER";
+  // This is lesson 2 ^
   // how can we change the MainText?
-   // how can we make the MainDiv larger instead of a circle?    
+  // how can we make the MainDiv larger instead of a circle?
 }
 
 // These are Event Listeners
